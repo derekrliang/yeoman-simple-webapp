668a5a357c01f094092715bba9d9eba275839d3c
Derek Liang <frostyrusty@gmail.com>
2014-10-29 22:52:44 -0700

    added todo-list basics, angularjs click/submit and data manipulation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name simpleWebappApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the simpleWebappApp
- */
 angular.module('simpleWebappApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+        $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+
+        // Submit callback
+        $scope.addTodo = function () {
+            $scope.todos.push($scope.todo);
+            $scope.todo = '';
+        };
+
+        $scope.removeTodo = function (index) {
+            $scope.todos.splice(index, 1);
+        };
+
   });
